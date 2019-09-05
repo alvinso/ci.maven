0218e6e57b61799850cba3d7ea90769bd4b93273
Tayyab Waqar <tayyab.waqar@ibm.com>
2019-09-05 14:21:34 -0400

    Enable command line invocation for install-feature without project pom

--- liberty-maven-plugin/src/main/java/io/openliberty/tools/maven/server/InstallFeatureMojo.java
+++ liberty-maven-plugin/src/main/java/io/openliberty/tools/maven/server/InstallFeatureMojo.java
@@ -15,16 +15,30 @@
  */
 package io.openliberty.tools.maven.server;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+
 import java.text.MessageFormat;
 import java.util.ArrayList;
+
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import javax.xml.parsers.DocumentBuilderFactory;
+
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 import io.openliberty.tools.ant.InstallFeatureTask;
 import io.openliberty.tools.ant.FeatureManagerTask.Feature;
@@ -38,20 +52,33 @@ import io.openliberty.tools.common.plugins.util.PluginScenarioException;
  * This mojo installs a feature packaged as a Subsystem Archive (esa) to the
  * runtime.
  */
-@Mojo(name = "install-feature")
+@Mojo(name = "install-feature", requiresProject = false)
 public class InstallFeatureMojo extends BasicSupport {
-    
+
     /**
-     * Define a set of features to install in the server and the configuration
-     * to be applied for all instances.
+     * Define a set of features to install in the server and the configuration to be
+     * applied for all instances.
      */
     @Parameter
     private Features features;
-    
+
+    @Parameter(property = "serverXmlFile")
+    private String serverXmlFile; // multiple features for CLI invocations
+
+    @Parameter(property = "acceptLicense")
+    private String acceptLicense;
+
+    @Parameter(property = "feature")
+    private String feature; // single feature for CLI invocations
+
+    private final String STANDALONE_GROUPID = "org.apache.maven";
+    private final String STANDALONE_ARTIFACTID = "standalone-pom";
+
     private boolean noFeaturesSection;
 
     private class InstallFeatureMojoUtil extends InstallFeatureUtil {
-        public InstallFeatureMojoUtil(Set<String> pluginListedEsas)  throws PluginScenarioException, PluginExecutionException {
+        public InstallFeatureMojoUtil(Set<String> pluginListedEsas)
+                throws PluginScenarioException, PluginExecutionException {
             super(installDirectory, features.getFrom(), features.getTo(), pluginListedEsas);
         }
 
@@ -59,17 +86,17 @@ public class InstallFeatureMojo extends BasicSupport {
         public void debug(String msg) {
             log.debug(msg);
         }
-        
+
         @Override
         public void debug(String msg, Throwable e) {
             log.debug(msg, e);
         }
-        
+
         @Override
         public void debug(Throwable e) {
             log.debug(e);
         }
-        
+
         @Override
         public void warn(String msg) {
             log.warn(msg);
@@ -79,14 +106,15 @@ public class InstallFeatureMojo extends BasicSupport {
         public void info(String msg) {
             log.info(msg);
         }
-        
+
         @Override
         public boolean isDebugEnabled() {
             return log.isDebugEnabled();
         }
-        
+
         @Override
-        public File downloadArtifact(String groupId, String artifactId, String type, String version) throws PluginExecutionException {
+        public File downloadArtifact(String groupId, String artifactId, String type, String version)
+                throws PluginExecutionException {
             try {
                 return getArtifact(groupId, artifactId, type, version).getFile();
             } catch (MojoExecutionException e) {
@@ -94,9 +122,10 @@ public class InstallFeatureMojo extends BasicSupport {
             }
         }
     }
-        
+
     /*
      * (non-Javadoc)
+     * 
      * @see org.codehaus.mojo.pluginsupport.MojoSupport#doExecute()
      */
     @Override
@@ -107,22 +136,88 @@ public class InstallFeatureMojo extends BasicSupport {
 
         if (features == null) {
             // For liberty-assembly integration:
-            // When using installUtility, if no features section was specified, 
+            // When using installUtility, if no features section was specified,
             // then don't install features because it requires license acceptance
             noFeaturesSection = true;
-            
+
             // initialize features section for all scenarios except for the above
             features = new Features();
         }
 
         checkServerHomeExists();
+
+        if (isRunningCliWithoutPom(getProject())) {
+            // running with pom
+            System.out.println("Running without pom");
+            addCommandLineFeatures();
+        }
         installFeatures();
+
     }
 
-    private void installFeatures() throws PluginExecutionException {       
+    // validate parameters for command line installs
+    private boolean validateParameters() {
+        if (installDirectory != null && acceptLicense != null && acceptLicense.equals("true")
+                && (serverXmlFile != null | feature != null)) {
+            return true;
+        }
+        return false;
+    }
+
+    private void addCommandLineFeatures() throws MojoExecutionException {
+        if (validateParameters()) {
+            parseSingleFeature();
+            parseServerXmlForFeatures();
+
+        } else {
+            throw new MojoExecutionException(
+                    "Invalid parameters for command line mode. Ensure you pass in installDirectory,serverXmlFile or feature, and acceptLicense = true as the parameters");
+
+        }
+    }
+
+    private void parseSingleFeature() throws MojoExecutionException {
+        if (feature == null) {
+            return;
+        }
+
+        String[] split = feature.split(":");
+        if (split.length == 1) { // feature shortname
+            features.addFeature(split[0]);
+        }
+        if (split.length == 2) {
+            // groupid:artifactid
+            features.addFeature(split[1]);
+        }
+        if (split.length == 3) {
+            // groupid:artifactid:version
+            String version = split[2];
+            if (version.equals(detectRuntimeVersion())) {
+                features.addFeature(split[1]);
+            } else {
+                throw new MojoExecutionException("Feature version doesnt match runtime version");
+            }
+        }
+
+        if (split.length == 4) {
+            // groupid:artifactid:type:version
+            String type = split[2];
+            String version = split[3];
+            if ("esa".equals(type) && version.equals(detectRuntimeVersion())) {
+                features.addFeature(split[1]);
+            } else {
+                throw new MojoExecutionException("Feature version doesnt match runtime version or type not esa");
+                // TODO: change later to seperate exception from type and version
+            }
+
+        }
+
+    }
+
+    private void installFeatures() throws PluginExecutionException {
         Set<String> pluginListedFeatures = getPluginListedFeatures(false);
         Set<String> pluginListedEsas = getPluginListedFeatures(true);
-        
+
         InstallFeatureUtil util;
         try {
             util = new InstallFeatureMojoUtil(pluginListedEsas);
@@ -142,11 +237,12 @@ public class InstallFeatureMojo extends BasicSupport {
         Set<String> dependencyFeatures = getDependencyFeatures();
         Set<String> serverFeatures = serverDirectory.exists() ? util.getServerFeatures(serverDirectory) : null;
 
-        Set<String> featuresToInstall = InstallFeatureUtil.combineToSet(pluginListedFeatures, dependencyFeatures, serverFeatures);
+        Set<String> featuresToInstall = InstallFeatureUtil.combineToSet(pluginListedFeatures, dependencyFeatures,
+                serverFeatures);
 
         util.installFeatures(features.isAcceptLicense(), new ArrayList<String>(featuresToInstall));
     }
-    
+
     private Set<String> getPluginListedFeatures(boolean findEsaFiles) {
         Set<String> result = new HashSet<String>();
         for (Feature feature : features.getFeatures()) {
@@ -158,11 +254,11 @@ public class InstallFeatureMojo extends BasicSupport {
         }
         return result;
     }
-    
+
     private Set<String> getDependencyFeatures() {
         Set<String> result = new HashSet<String>();
         List<org.apache.maven.model.Dependency> dependencyArtifacts = project.getDependencies();
-        for (org.apache.maven.model.Dependency dependencyArtifact: dependencyArtifacts){
+        for (org.apache.maven.model.Dependency dependencyArtifact : dependencyArtifacts) {
             if (("esa").equals(dependencyArtifact.getType())) {
                 result.add(dependencyArtifact.getArtifactId());
                 log.debug("Dependency feature: " + dependencyArtifact.getArtifactId());
@@ -177,7 +273,8 @@ public class InstallFeatureMojo extends BasicSupport {
                 .createTask("antlib:io/openliberty/tools/ant:install-feature");
 
         if (installFeatureTask == null) {
-            throw new IllegalStateException(MessageFormat.format(messages.getString("error.dependencies.not.found"), "install-feature"));
+            throw new IllegalStateException(
+                    MessageFormat.format(messages.getString("error.dependencies.not.found"), "install-feature"));
         }
 
         installFeatureTask.setInstallDir(installDirectory);
@@ -191,6 +288,65 @@ public class InstallFeatureMojo extends BasicSupport {
         installFeatureTask.setFeatures(installFeatures);
         installFeatureTask.setFrom(features.getFrom());
         installFeatureTask.execute();
+    }
+
+    private boolean isRunningCliWithoutPom(MavenProject project) {
+        if (project.getFile() == null && project.getGroupId().equals(STANDALONE_GROUPID)
+                && project.getArtifactId().equals(STANDALONE_ARTIFACTID)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private void parseServerXmlForFeatures() throws MojoExecutionException {
+        // TODO: REUSE below to get server xml features
+        // io.openliberty.tools.common.plugins.util.ServerFeatureUtil.getServerXmlFeatures()
+
+        if (serverXmlFile != null) {
+            try {
+                InputStream serverXMLInputStream = new FileInputStream(serverXmlFile);
+                Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(serverXMLInputStream);
+                Element element = doc.getDocumentElement();
+                NodeList fmList = element.getElementsByTagName("featureManager");
+                for (int i = 0; i < fmList.getLength(); i++) {
+                    Node fm = fmList.item(i);
+                    Element fmElement = (Element) fm;
+                    NodeList fList = fmElement.getElementsByTagName("feature");
+                    for (int j = 0; j < fList.getLength(); j++) {
+                        Node f = fList.item(j);
+                        features.addFeature(f.getTextContent().trim());
+                    }
+                }
+            } catch (Exception e) {
+                throw new MojoExecutionException(e.getMessage());
+            }
+        }
+
+
+    }
+
+    private String detectRuntimeVersion() throws MojoExecutionException {
+        String sep = System.getProperty("os.name").toLowerCase().indexOf("win") >= 0 ? "\\" : "/"; // get file seperator
+        String openLibProperties = installDirectory + sep + "lib" + sep + "versions" + sep + "openliberty.properties";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(openLibProperties))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                if(line.startsWith("com.ibm.websphere.productVersion")) {
+                    return line.split("=")[1].trim();
+                }
+            }
+
+        } catch (IOException e) {
+            throw new MojoExecutionException(e.getMessage());
+        }
+
+        return null;
+
+
+
+
     }
 
 }
